636567d1f61c91505d1f069dde6211eadf36d080
zhengyanhu <xiaohuzyh@icloud.com>
2016-10-19 16:13:47 +0800

    点击tabbar实现界面跳转

--- dangdang-app-angular/assets/controllers/main_controller.js
+++ dangdang-app-angular/assets/controllers/main_controller.js
@@ -1,6 +1,9 @@
 app.controller('mainContoller',['$scope','commonService',function($scope,c_s){
   c_s.getData('anime',function(res){
     console.dir(res);
-  })
-
+  })；
+  $scope.selectedIndex = 0;
+  $scope.linkTo = function(index){
+    $scope.selectedIndex = index;
+  }
 }])
