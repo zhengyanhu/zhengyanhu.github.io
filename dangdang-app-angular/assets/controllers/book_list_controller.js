388da968cd8413864caf36dfafe1dd08cdb36869
zhengyanhu <xiaohuzyh@icloud.com>
2016-10-19 21:50:22 +0800

    1

--- dangdang-app-angular/assets/controllers/book_list_controller.js
+++ dangdang-app-angular/assets/controllers/book_list_controller.js
@@ -1,7 +1,7 @@
 app.controller('book_listController',['$scope','$routeParams','commonService',function($scope,$routeParams,commonService){
  $scope.data = [];
  commonService.getData($routeParams.id,function(res){
-   $scope.data = res.data.data;
+   $scope.data = res.data;
    console.log(res.data);
  });
 }]);
