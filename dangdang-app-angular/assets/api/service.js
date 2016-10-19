387dea970258083b01c69b7fa7c853adb9fceb09
zhengyanhu <xiaohuzyh@icloud.com>
2016-10-19 15:54:09 +0800

    修复错误

--- dangdang-app-angular/assets/api/service.js
+++ dangdang-app-angular/assets/api/service.js
@@ -1,6 +1,6 @@
 //创建一个公用的数据调用服务
-app.factory('commonService',['$http'function($http){
-  var service={};
+app.factory('commonService',['$http',function($http){
+  var service = {};
   //根据分类信息取数据
   //type 数据类型
   service.getData = function(type,callBack){
