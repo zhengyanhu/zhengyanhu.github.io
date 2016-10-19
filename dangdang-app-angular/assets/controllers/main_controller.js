622a77c6bc48fa95cd7260acbb1865263e783b09
zhengyanhu <xiaohuzyh@icloud.com>
2016-10-19 16:16:46 +0800

    修改错误

--- dangdang-app-angular/assets/controllers/main_controller.js
+++ dangdang-app-angular/assets/controllers/main_controller.js
@@ -1,7 +1,7 @@
 app.controller('mainContoller',['$scope','commonService',function($scope,c_s){
-  c_s.getData('anime',function(res){
-    console.dir(res);
-  })；
+  // c_s.getData('anime',function(res){
+  //   console.dir(res);
+  // });
   $scope.selectedIndex = 0;
   $scope.linkTo = function(index){
     $scope.selectedIndex = index;
