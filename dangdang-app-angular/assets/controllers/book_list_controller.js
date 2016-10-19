d0fc3a05b983a2f949b67cf76901b14650574620
zhengyanhu <xiaohuzyh@icloud.com>
2016-10-19 20:39:44 +0800

    加入图书数据

---
+++ dangdang-app-angular/assets/controllers/book_list_controller.js
@@ -0,0 +1,6 @@
+app.controller('book_listController',['$scope','$routeParams','commonService',function($scope,$routeParams,commonService){
+ $scope.data = [];
+ commonService.getData($routeParams.id,function(res){
+   $scope.data = res.data.data;
+ });
+}]);
