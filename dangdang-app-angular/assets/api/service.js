2adf18031ab8534b25e79a2df0fd37d5c142e892
zhengyanhu <xiaohuzyh@icloud.com>
2016-10-19 21:31:20 +0800

    1

--- dangdang-app-angular/assets/api/service.js
+++ dangdang-app-angular/assets/api/service.js
@@ -4,6 +4,7 @@ app.factory('commonService',['$http',function($http){
   //根据分类信息取数据
   //type 数据类型
   service.getData = function(type,callBack){
+    console.log(type);
     $http({
       url:'/dangdang-app-angular/data/book_'+type+'.json',
       method:'get'
--- dangdang-app-angular/assets/controllers/book_controller.js
+++ dangdang-app-angular/assets/controllers/book_controller.js
@@ -1,8 +1,8 @@
 app.controller('bookContrller',['$scope',function($scope){
   $scope.bookTypes = [
-    {id:'anime',name:'动漫',img:'',description:'最近7天热卖动漫类书籍'},
-    {id:'childern',name:'儿童',img:'',description:'最近7天热卖儿童类书籍'},
-    {id:'history',name:'历史',img:'',description:'最近7天热卖历史类书籍'},
-    {id:'youthLierature',name:'青春文学',img:'',description:'最近7天热卖青春文学类书籍'},
+    {id:'anime',name:'动漫',img:'http://img3x8.ddimg.cn/20/27/24042368-1_l_6.jpg',description:'最近7天热卖动漫类书籍'},
+    {id:'childern',name:'儿童',img:'http://img3x3.ddimg.cn/29/32/24033863-1_l_6.jpg',description:'最近7天热卖儿童类书籍'},
+    {id:'history',name:'历史',img:'http://img3x5.ddimg.cn/24/35/24025245-1_l_4.jpg',description:'最近7天热卖历史类书籍'},
+    {id:'youthLierature',name:'青春文学',img:'http://img3x4.ddimg.cn/34/5/24024364-1_l_12.jpg',description:'最近7天热卖青春文学类书籍'},
   ]
 }])
