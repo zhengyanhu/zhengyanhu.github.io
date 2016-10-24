090ef5adf1524c85c7a214aff5fc928b1ce65a57
zhengyanhu <xiaohuzyh@icloud.com>
2016-10-24 15:05:18 +0800

    代码统计

---
+++ 2016-10-24/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('./pulic'));
+
+
+app.get('/',function(req,res){
+    
+});
+
+app.listen(3000,function(){
+
+  console.log('服务器运行于3000端口');
+})
